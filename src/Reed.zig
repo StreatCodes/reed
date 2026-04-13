b95db7c6800cde5b41f5c96db17a15ca4483240a
Matthew Streatfield <matt.r.streatfield@gmail.com>
2026-04-13 23:24:25 +1000

    register river objects

--- src/Reed.zig
+++ src/Reed.zig
@@ -1,3 +1,4 @@
+const std = @import("std");
 const wayland = @import("wayland");
 const river = wayland.client.river;
 const wl = wayland.client.wl;
@@ -5,13 +6,52 @@ const wl = wayland.client.wl;
 const Reed = @This();
 
 display: *wl.Display,
+registry: *wl.Registry,
+window_manager: ?*river.WindowManagerV1 = null,
+xkb_bindings: ?*river.XkbBindingsV1 = null,
+layer_shell: ?*river.LayerShellV1 = null,
 
 pub fn init() !Reed {
-    return .{
-        .display = try wl.Display.connect(null),
+    const display = try wl.Display.connect(null);
+    const registry = try display.getRegistry();
+
+    var reed = Reed{
+        .display = display,
+        .registry = registry,
     };
+
+    registry.setListener(*Reed, registryListener, &reed);
+    if (display.roundtrip() != .SUCCESS) return error.RoundtripFailed;
+
+    return reed;
 }
 
 pub fn deinit(reed: *Reed) void {
+    if (reed.layer_shell) |layer_shell| layer_shell.destroy();
+    if (reed.xkb_bindings) |xkb_bindings| xkb_bindings.destroy();
+    if (reed.window_manager) |window_manager| window_manager.destroy();
+    reed.registry.destroy();
     reed.display.disconnect();
+}
+
+fn registryListener(registry: *wl.Registry, event: wl.Registry.Event, reed: *Reed) void {
+    switch (event) {
+        .global => |global| {
+            const interface_name = std.mem.span(global.interface);
+            if (std.mem.eql(u8, interface_name, "river_window_manager_v1")) {
+                std.debug.print("Registering {s}\n", .{interface_name});
+                reed.window_manager =
+                    registry.bind(global.name, river.WindowManagerV1, 4) catch null;
+            } else if (std.mem.eql(u8, interface_name, "river_xkb_bindings_v1")) {
+                std.debug.print("Registering {s}\n", .{interface_name});
+                reed.xkb_bindings =
+                    registry.bind(global.name, river.XkbBindingsV1, 2) catch null;
+            } else if (std.mem.eql(u8, interface_name, "river_layer_shell_v1")) {
+                std.debug.print("Registering {s}\n", .{interface_name});
+                reed.layer_shell =
+                    registry.bind(global.name, river.LayerShellV1, 1) catch null;
+            }
+        },
+        .global_remove => {},
+    }
 }
